ca0ad80181707d251323b944818ca01bb2a63651
adrianow795 <adrianow795@gmail.com>
2025-04-30 11:11:24 +0200

    Add 1st test

--- homework/shared_ptr/shared_ptr_tests.cpp
+++ homework/shared_ptr/shared_ptr_tests.cpp
@@ -1,3 +1,12 @@
+#include <gtest/gtest.h>
 #include "shared_ptr.hpp"
 
 template class my::shared_ptr<int>;
+
+
+TEST(SharedPtr, shouldCreateSharedPtrWithInitialValue)
+{
+    my::shared_ptr<int> p {new int(5)};
+    ASSERT_EQ(*p, 5);
+
+}
